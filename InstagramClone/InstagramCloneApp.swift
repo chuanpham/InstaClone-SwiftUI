c5f50e93e85b41a196d672459ab0d7396b964592
chuanpham <pvchuan2998@gmail.com>
2024-04-09 13:47:13 +0700

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by chuanpham on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
